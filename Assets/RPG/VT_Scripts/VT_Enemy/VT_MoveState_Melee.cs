6df7e2cd465e54898bdb9eaaac4f966a3215a9ce
Van-Tan-15081998 <leetaantig@gmail.com>
2026-03-20 08:41:38 +0700

    update a2

--- Assets/RPG/VT_Scripts/VT_Enemy/VT_MoveState_Melee.cs
+++ Assets/RPG/VT_Scripts/VT_Enemy/VT_MoveState_Melee.cs
@@ -5,6 +5,7 @@ using UnityEngine;
 public class VT_MoveState_Melee : VT_EnemyState
 {
     private VT_Enemy_Melee enemey;
+    private Vector3 destination;
 
     public VT_MoveState_Melee(VT_Enemy enemyBase, VT_EnemyStateMachine stateMachine, string animBoolName) : base(enemyBase, stateMachine, animBoolName)
     {
@@ -14,20 +15,20 @@ public class VT_MoveState_Melee : VT_EnemyState
     public override void Enter()
     {
         base.Enter();
+
+        destination = enemey.GetPatrolDestination();
     }
 
     public override void Exit()
     {
         base.Exit();
-
-        Debug.LogWarning("I exit move state");
     }
 
     public override void Update()
     {
         base.Update();
 
-        enemey.agent.SetDestination(enemey.destination.position);
+        enemey.agent.SetDestination(destination);
 
         if (enemey.agent.remainingDistance <= 1)
         {
